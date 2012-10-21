f945758c688678a991a5eb6dcee7074df4199671
Alexandre Cardoso <accbel@gmail.com>
2012-10-20 23:33:51 -0300

    basic chat now fully operational

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,27 +1,80 @@
 var room = {
+  init: function(){
+    var room = this;
+
+    this.input = $('textarea');
+    this.board = $('#board');
+    this.members = $('.members ul');
+
+    this.input.keypress(function(){
+      if ( event.which == 13 ) {
+        room.sendMessage();
+        return false;
+      }
+    });
+
+    room.owner = prompt('Qual o seu nome?');
+
+    room.open();
+  },
+  open: function(){
+    var room = this;
+    room.socket = io.connect('http://localhost:8080');
+
+    room.socket.on('connect', function () {
+      room.socket.emit('new',{name: room.owner},function(clients){
+        room.setMembersList(clients);
+      });
+    });
+
+    room.socket.on('new',function(who){
+      room.appendNewMember(who);
+    });
+
+    room.socket.on('leave',function(who){
+      room.dropMember(who);
+    });
+
+    room.socket.on('message',function(update){
+      room.printMessage(update.author,update.message);
+    });
+  },
+  setMembersList: function(members){
+    var list = '';
+    for (var i in members) {
+      list += '<li>'+members[i]+'</li>';
+    };
+
+    this.members.html(list);
+  },
+  appendNewMember: function(who) {
+    this.members.append('<li>'+who+'</li>');
+  },
+  dropMember: function(who){
+    this.members.find("li:contains('"+who+"')").remove();
+  },
+  printMessage: function(author,message) {
+    var newMessage = $('<dl></dl>');
+    var authorTag = $('<dt></dt>');
+    var messageTag = $('<dd></dd>');
+
+    authorTag.html(author);
+    messageTag.html(message);
+    newMessage.append(authorTag);
+    newMessage.append(messageTag);
+
+    this.board.append(newMessage);
+  },
   sendMessage: function() {
-    var input = $('textarea');
-    
-    var newLine = $('<dl></dl>');
-    var author = $('<dt></dt>');
-    var message = $('<dd></dd>');
+    var message = this.input.val();
 
-    author.html('Eu');
-    message.html(input.val());
-    newLine.append(author);
-    newLine.append(message);
+    this.socket.json.send({author: this.owner,message: message});
+    this.printMessage(this.owner,message);
 
-    $('#board').append(newLine);
-
-    input.val("");
+    this.input.val('');
   }
 };
 
 $(function() {
-  $("textarea").keypress(function(event) {
-    if ( event.which == 13 ) {
-      room.sendMessage();
-      return false;
-    }
-  });
+  room.init();
 });
--- server.js
+++ server.js
@@ -2,9 +2,31 @@ var connect = require('connect')
   , http = require('http')
   , io = require('socket.io');
 
+var clients = [];
+
 var app = connect().use(connect.static('public'));
 var server = http.createServer(app);
-
-io.listen(server);
+io = io.listen(server);
 
 server.listen(8080);
+
+io.sockets.on('connection', function (socket) {
+  socket.on('message', function(data,callback){
+    socket.broadcast.json.send(data);
+  });
+
+  socket.on('new', function (data,callback) {
+    socket.set('nickname',data.name);
+    clients.push(data.name);
+    socket.broadcast.emit('new',data.name);
+    callback(clients);
+  });
+
+  socket.on('disconnect',function(){
+    socket.get('nickname',function(err,name){
+      var index = clients.indexOf(name);
+      clients.splice(index, 1);
+      socket.broadcast.emit('leave',name);
+    });
+  });
+});
